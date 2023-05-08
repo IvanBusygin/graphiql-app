5f867e750c4ba14191d77bceb41202f60f17497f
fliderman01 <fliderman01@gmail.com>
2023-05-08 12:48:31 +0400

    feat/404 route

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
 export function NotFoundPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-slate-300">NotFoundPage</div>
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center text-yellow-800">
+      <h1 className="text-9xl font-black">404</h1>
+      <p className="mb-11 text-4xl">It looks like you&apos;re lost</p>
+      <Link
+        className="relative rounded bg-yellow-800 px-3.5 py-3.5 text-white active:left-1 active:top-1"
+        to="/"
+      >
+        Go to the homepage
+      </Link>
+    </div>
   );
 }
