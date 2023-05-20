f0d2d3a9b608230d512b778df5a71a40ce3f8227
Andrei Rassokhin <rasosha@gmail.com>
2023-05-20 13:49:30 +0800

    modal

--- src/components/Form/LoginForm.tsx
+++ src/components/Form/LoginForm.tsx
@@ -6,8 +6,9 @@ import { Link } from 'react-router-dom';
 import EmailInput from './EmailInput';
 import PasswordInput from './PasswordInput';
 import { logInWithEmailAndPassword } from '../../firebase';
-import { ModalMsg } from '../../UI/ModalMsg';
 import { FormInputs } from './interfaces';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const LoginForm = () => {
   const {
@@ -15,7 +16,6 @@ export const LoginForm = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<FormInputs>({ mode: 'onSubmit', reValidateMode: 'onSubmit' });
-  const [isModal, setIsModal] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const onSubmit = async (data: FormInputs) => {
@@ -23,48 +23,56 @@ export const LoginForm = () => {
       setIsLoading(true);
       await logInWithEmailAndPassword(data.email, data.password);
     } catch (err) {
+      toast.error(err as string);
       console.error('error :>> ', err);
-      setIsModal(err as string);
     }
     setIsLoading(false);
   };
   return (
-    <div className="border-1 relative flex w-full max-w-xs flex-col items-center rounded-xl border-gray-100 bg-gray-50 p-8 drop-shadow-lg">
-      {!isModal && <CloseBtn />}
-      <ModalMsg
-        isOpen={isModal !== ''}
-        title="Ошибка авторизации:"
-        text={isModal}
-        setIsModal={setIsModal}
+    <section className="flex w-full items-center justify-center">
+      <ToastContainer
+        position="top-center"
+        autoClose={10000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
       />
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex w-full flex-col"
-      >
-        <EmailInput
-          register={register}
-          errors={errors}
-        />
-        <div className="flex w-full flex-col">
-          <PasswordInput
+      <div className="border-1 relative flex w-full max-w-xs flex-col items-center rounded-xl border-gray-100 bg-gray-50 p-8 drop-shadow-lg">
+        <CloseBtn />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex w-full flex-col"
+        >
+          <EmailInput
             register={register}
             errors={errors}
-            recoverLink
           />
-        </div>
-        <ActionBtn
-          btnText="Log in"
-          isLoading={isLoading}
-        />
-      </form>
-      <p className="text-gray-400">Don&apos;t have account?</p>
-      <Link
-        tabIndex={5}
-        to="/register"
-        className="text-blue-500 hover:text-blue-700"
-      >
-        Register now.
-      </Link>
-    </div>
+          <div className="flex w-full flex-col">
+            <PasswordInput
+              register={register}
+              errors={errors}
+              recoverLink
+            />
+          </div>
+          <ActionBtn
+            btnText="Log in"
+            isLoading={isLoading}
+          />
+        </form>
+        <p className="text-gray-400">Don&apos;t have account?</p>
+        <Link
+          tabIndex={5}
+          to="/register"
+          className="text-blue-500 hover:text-blue-700"
+        >
+          Register now.
+        </Link>
+      </div>
+    </section>
   );
 };
