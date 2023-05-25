3583197fd9d141fc31468f7cc47bf740f76fe6d5
fliderman01 <fliderman01@gmail.com>
2023-05-25 12:39:01 +0400

    fix: redirection issue on page refresh

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,8 @@
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import { auth } from '../firebase';
 
+import { auth } from '../firebase';
 import GraphiqlEditor from '../components/Editor/GraphqlEditor';
 
 export const MainPage = () => {
