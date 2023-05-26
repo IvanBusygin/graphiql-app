f1d61241d7335055d8fa29f00e325511a2199781
IvanBusygin <bi-25@yandex.ru>
2023-05-26 16:48:45 +0400

    refactor: changed the appearance of the welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -3,10 +3,14 @@ import { auth } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useTranslation } from 'react-i18next';
 import { ReactComponent as Beaker } from '../assets/Buttons/Beaker.svg';
+import { ReactComponent as Key } from '../assets/Buttons/Key.svg';
 
 export function WelcomePage() {
   const [user, loading] = useAuthState(auth);
   const { t } = useTranslation();
+
+  const btnStyles =
+    'm-2 flex h-20 w-44 items-center justify-center rounded-lg bg-[#2A9E76] hover:bg-[#2BAB7C]';
 
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center bg-grayLight">
@@ -18,30 +22,29 @@ export function WelcomePage() {
           <p>loading...</p>
         </>
       ) : !user ? (
-        <>
+        <p className="flex flex-col items-center justify-center text-white">
           <p className="m-2">To use GraphiQL you need to log in.</p>
-          <p className="m-2">
-            Go to the{' '}
-            <Link
-              to="/login"
-              className="text-blue-700"
-            >
-              login page
-            </Link>
-          </p>
-        </>
-      ) : (
-        <>
-          <p className="m-2 text-white">Now you can go to the Editor</p>
           <Link
-            className="m-2 flex h-20 w-44 items-center justify-center rounded-lg bg-[#2A9E76] hover:bg-[#2BAB7C]"
+            className={btnStyles}
+            to="/login"
+            title="Go to the login page"
+          >
+            <Key />
+            <p className="ml-2 ">Enter login</p>
+          </Link>
+        </p>
+      ) : (
+        <p className="flex flex-col items-center justify-center text-white">
+          <p className="m-2">Now you can go to the Editor</p>
+          <Link
+            className={btnStyles}
             to="/main"
             title="To GraphiQL API Editor"
           >
             <Beaker />
-            <p className="ml-2">GraphiQL Editor</p>
+            <p className="ml-2 ">GraphiQL Editor</p>
           </Link>
-        </>
+        </p>
       )}
     </div>
   );
