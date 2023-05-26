2dcb4e49dc99953266b65b85bad510e23417bf4c
Andrei Rassokhin <rasosha@gmail.com>
2023-05-27 01:56:15 +0800

    feat: add info about project, course and us

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -10,11 +10,11 @@ export function WelcomePage() {
   const { t } = useTranslation();
 
   const btnStyles =
-    'm-2 flex h-20 w-48 items-center justify-center rounded-lg bg-[#2A9E76] hover:bg-[#2BAB7C]';
+    'px-4 py-6 m-4 flex h-10 items-center justify-center rounded-lg bg-[#2A9E76] hover:bg-[#2BAB7C]';
 
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-grayLight">
-      <h1 className="m-2 text-center text-3xl font-bold text-[#2A9E76] underline">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center  bg-grayLight align-middle text-lg text-white">
+      <h1 className="m-10 text-center text-3xl font-bold text-[#2A9E76] underline">
         {t('welcome')}
       </h1>
       {loading ? (
@@ -22,7 +22,7 @@ export function WelcomePage() {
           <p>loading...</p>
         </>
       ) : !user ? (
-        <p className="flex flex-col items-center justify-center text-white">
+        <div className="flex flex-col items-center justify-center text-white">
           <p className="m-2"> {t('toLogIn')} </p>
           <Link
             className={btnStyles}
@@ -32,7 +32,7 @@ export function WelcomePage() {
             <Key />
             <p className="ml-2 "> {t('btnToLogIn')} </p>
           </Link>
-        </p>
+        </div>
       ) : (
         <p className="flex flex-col items-center justify-center text-white">
           <p className="m-2"> {t('toEditor')} </p>
@@ -46,6 +46,60 @@ export function WelcomePage() {
           </Link>
         </p>
       )}
+      <div className="mt-20 flex w-full flex-col items-center gap-20">
+        <div className="flex w-[75%] max-w-[1000px] flex-wrap justify-center gap-4 text-center">
+          <h1 className="text-3xl">{t('aboutProjectTitle')}</h1>
+          <p className="rounded-xl bg-grayDark p-4">{t('aboutProjectText')}</p>
+        </div>
+        <div className="flex w-[75%] max-w-[1000px] flex-wrap justify-center gap-4 text-center">
+          <h1 className="text-3xl">{t('aboutRSSchoolTitle')}</h1>
+          <p className="rounded-xl bg-grayDark p-4">{t('aboutRSSchoolText')}</p>
+        </div>
+        <div className="mb-10 flex flex-col text-center">
+          <h1 className="mb-5 text-3xl">{t('aboutUs')}</h1>
+          <div className="flex flex-wrap justify-center gap-10 rounded-xl bg-grayDark p-10">
+            <Link
+              to="https://github.com/IvanBusygin"
+              className="group relative flex h-[250px] w-[250px] shrink-0 overflow-hidden rounded-full shadow-2xl ring-2 ring-darkRed transition-all duration-500 hover:ring-8"
+            >
+              <img
+                src="https://avatars.githubusercontent.com/u/88594437?v=4"
+                alt="Ivan Busygin"
+                className=" brightness-100  transition-all duration-500 group-hover:scale-125  group-hover:brightness-[10%]"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-full grid place-items-center text-3xl underline transition-all duration-500 group-hover:top-0">
+                Ivan <br /> Busygin
+              </div>
+            </Link>
+            <Link
+              to="https://github.com/BesoBerdzenishvili"
+              className="group relative flex h-[250px] w-[250px] shrink-0 overflow-hidden rounded-full shadow-2xl ring-2 ring-darkRed transition-all duration-500 hover:ring-8"
+            >
+              <img
+                src="https://avatars.githubusercontent.com/u/76405148?v=4"
+                alt="Beso Berdzenishvili"
+                className=" brightness-100  transition-all duration-500 group-hover:scale-125  group-hover:brightness-[10%]"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-full grid place-items-center text-3xl underline transition-all duration-500 group-hover:top-0">
+                Beso <br /> Berdzenishvili
+              </div>
+            </Link>
+            <Link
+              to="https://github.com/rasosha"
+              className="group relative flex h-[250px] w-[250px] shrink-0 overflow-hidden rounded-full shadow-2xl ring-2 ring-darkRed transition-all duration-500 hover:ring-8"
+            >
+              <img
+                src="https://media.licdn.com/dms/image/D5603AQH3WcAMmUBIew/profile-displayphoto-shrink_800_800/0/1683787710715?e=1690416000&v=beta&t=lCDSEFVVFfofzcpsH6bnCuXqW7opWYj9WDOkw73rNco"
+                alt="rasosha"
+                className=" brightness-100  transition-all duration-500 group-hover:scale-125  group-hover:brightness-[10%]"
+              />
+              <div className="absolute bottom-0 left-0 right-0 top-full grid place-items-center text-3xl underline transition-all duration-500 group-hover:top-0">
+                Andrei <br /> Rassokhin
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
