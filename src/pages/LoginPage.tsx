a65118ef3f6a706edee081081190a2da7ec85a04
Andrei Rassokhin <rasosha@gmail.com>
2023-05-28 23:12:19 +0800

    fix: fix styles

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '../components/Form/LoginForm';
 
 export function LoginPage() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-full w-full grow items-center justify-center">
       <LoginForm />
     </div>
   );
